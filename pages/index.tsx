be407b55475ae0d6d54321bf17892ec4df95140d
Rhea Dieberger <rdieberger@gmail.com>
2023-01-03 17:22:17 +0100

    Correct sorting of posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export type Post = {
   title: string;
   slug: string;
   html: string;
+  feature_image: string;
+  feature_image_caption: string;
   tags: { name: string }[];
 };
 
@@ -16,7 +19,7 @@ type Props =
 
 async function getPosts() {
   const res = await fetch(
-    `${process.env.blog_url}/ghost/api/v3/content/posts/?key=${process.env.content_api_key}&fields=title,slug,html&include=tags`,
+    `${process.env.blog_url}/ghost/api/v3/content/posts/?key=${process.env.content_api_key}&fields=title,slug,html,feature_image,feature_image_caption&include=tags`,
   ).then((resp) => resp.json());
 
   const posts = res.posts;
@@ -52,51 +55,73 @@ export default function Home(props: Props) {
         <p>{props.error}</p>
       </>
     );
-  }
+  } else {
+    const postsSortedAsArrays: Post[][] = Object.values(
+      props.posts.reduce((acc, current) => {
+        acc[current.tags[1].name] = acc[current.tags[1].name] ?? [];
+        acc[current.tags[1].name].push(current);
+        return acc;
+      }, {}),
+    );
 
-  const sortedPosts = props.posts.sort((a, b) =>
-    a.tags[1].name > b.tags[1].name
-      ? 1
-      : a.tags[1].name === b.tags[1].name
-      ? a.tags[0].name > b.tags[0].name
-        ? 1
-        : -1
-      : -1,
-  );
-
-  return (
-    <>
-      <Head>
-        <title>Ann Website</title>
-        <meta name="description" content="Ann's Website in 3 languages" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="grid justify-items-center">
-        <div className="grid grid-cols-1 gap-x-16 gap-y-16 lg:grid-cols-3">
-          {sortedPosts.map((post) => {
+    return (
+      <>
+        <Head>
+          <title>Ann Website</title>
+          <meta name="description" content="Ann's Website in 3 languages" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="grid justify-items-stretch gap-y-16">
+          {postsSortedAsArrays.map((postGroup) => {
+            postGroup.sort((a, b) =>
+              a.tags[0].name > b.tags[0].name ? 1 : -1,
+            );
             return (
               <div
-                key={post.slug}
-                className={
-                  post.tags[0].name === 'de'
-                    ? 'lg:col-start-1'
-                    : post.tags[0].name === 'en'
-                    ? 'lg:col-start-2'
-                    : 'lg:col-start-3'
-                }
+                className="grid grid-cols-1 gap-x-16 lg:grid-cols-3 "
+                key={postGroup[0]?.tags[1]?.name}
               >
-                <Link href="/text/[slug]" as={`/text/${post.slug}`}>
-                  <h2 className="font-bold">{post.title}</h2>
-                </Link>
-                {/* <p>{post.tags[0].name}</p>
-                <p>{post.tags[1].name}</p> */}
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                {postGroup.map((post) => {
+                  return (
+                    <div
+                      key={post.slug}
+                      className={
+                        post.tags[0].name === 'de'
+                          ? 'lg:col-start-1 '
+                          : post.tags[0].name === 'en'
+                          ? 'lg:col-start-2'
+                          : 'lg:col-start-3'
+                      }
+                      lang={
+                        post.tags[0].name === 'de'
+                          ? 'de'
+                          : post.tags[0].name === 'en'
+                          ? 'en'
+                          : 'ja'
+                      }
+                    >
+                      {post.feature_image ? (
+                        <img
+                          alt={`Illustration for the text ${post.title}`}
+                          src={post.feature_image}
+                        />
+                      ) : null}
+                      {post.feature_image_caption ? (
+                        <p className="text-xs">{post.feature_image_caption}</p>
+                      ) : null}
+                      <Link href="/text/[slug]" as={`/text/${post.slug}`}>
+                        <h2 className="mt-4 font-bold ">{post.title}</h2>
+                      </Link>
+                      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                    </div>
+                  );
+                })}
               </div>
             );
           })}
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
